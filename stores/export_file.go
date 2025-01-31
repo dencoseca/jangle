331080f449f761661b0cc5063cfb24293e50ddd0
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2025-01-31 19:51:20 +0000

    Update variable name for clarity in export logic
    Renamed `targetLine` to `exportLine` to better reflect its purpose and improve code readability. This change ensures clearer and more intuitive understanding of the variable's role in the export process.

--- stores/export_file.go
+++ stores/export_file.go
@@ -41,12 +41,12 @@ func (cf ExportFile) Delete(name string) error {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	targetLine := fmt.Sprintf("export %s=$(jangle get %s)", name, name)
+	exportLine := fmt.Sprintf("export %s=$(jangle get %s)", name, name)
 
 	var lines []string
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.TrimSpace(line) != targetLine {
+		if strings.TrimSpace(line) != exportLine {
 			lines = append(lines, line)
 		}
 	}
